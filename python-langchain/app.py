4a96454aaaee097bd23d11c7f4ea203f996f05f1
James Kiper <jamesraykiper@hotmail.com>
2026-03-06 03:54:39 -0500

    Add orchestration scaffold and environment validation

---
+++ python-langchain/app.py
@@ -0,0 +1,15 @@
+import os
+import asyncio
+import json
+from typing import TypedDict, Annotated, Literal
+from dotenv import load_dotenv
+from langchain.agents import create_agent
+from langchain_mcp_adapters.client import MultiServerMCPClient
+from langchain_openai import ChatOpenAI
+from langchain_core.messages import HumanMessage
+from langgraph.graph import StateGraph, START
+from langgraph.graph.message import add_messages
+from langgraph.types import Command
+
+load_dotenv()
+print("\nOrchestration setup complete!")
